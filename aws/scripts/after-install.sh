56ba1e34837e9ea00f41d9d06e2dfb3b8f32fbf6
soohyun <developer.shkim@gmail.com>
2024-07-14 13:11:18 +0900

    fix: code deploy 중 에러 해결

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,9 +3,9 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp -r s3://##s3-bucket##/dist /usr/local/app/dist
-aws s3 cp -r s3://##s3-bucket##/tsconfig.json /usr/local/app/tsconfig.json
-aws s3 cp -r s3://##s3-bucket##/package.json /usr/local/app/package.json
+aws s3 cp s3://##s3-bucket##/dist /usr/local/app/dist --recursive
+aws s3 cp s3://##s3-bucket##/tsconfig.json /usr/local/app/tsconfig.json
+aws s3 cp s3://##s3-bucket##/package.json /usr/local/app/package.json
 
 cd /usr/local/app/
 
