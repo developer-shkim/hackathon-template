b939c756ef69cf4bb7b4a3e84e1c1ed611fd388b
soohyun <developer.shkim@gmail.com>
2024-07-14 15:45:30 +0900

    chore: s3 bucket, region 설정

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 set -xe
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install v20.15.1
+nvm use v20.15.1
+
+node -v
+npm -v
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+yarn --version
+
+apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/dist /usr/local/app/dist --recursive
-aws s3 cp s3://##s3-bucket##/tsconfig.json /usr/local/app/tsconfig.json
-aws s3 cp s3://##s3-bucket##/package.json /usr/local/app/package.json
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-rlpt97jfviqg/dist /usr/local/app/dist --recursive
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-rlpt97jfviqg/tsconfig.json /usr/local/app/tsconfig.json
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-rlpt97jfviqg/package.json /usr/local/app/package.json
 
 cd /usr/local/app/
 
